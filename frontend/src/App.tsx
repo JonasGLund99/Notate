45a71fe31284d45da91dfac18d8e95fd87015f0f
Jonas Geertsen Lund <jgl21@student.aau.dk>
2023-03-19 22:12:12 +0100

    Added favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './Notate-blue-rounded.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Hi. This is cool. I'm going to make a change to this file and see if it updates in the browser.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [clickCount, setClickCount] = useState(0);
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+                Hi. This is cool. I'm going to make a change to this file and see if it updates in the browser.
+            </p>
+            <Button onClick={() => setClickCount(clickCount + 1)}>
+                Clicked {clickCount} times
+            </Button> 
+        </header>
+        </div>
+    );
 }
 
 export default App;
