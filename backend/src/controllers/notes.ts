321232b38233cd40cdafe2f7b4c994d16dc3bf25
Jonas Geertsen Lund <jgl21@student.aau.dk>
2023-03-19 21:04:50 +0100

    Added endpoints for update and delete

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import NoteModel from "../models/note";
 import createHttpError from "http-errors";
 import mongoose from "mongoose";
+import note from "../models/note";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
     try {
@@ -16,16 +17,13 @@ export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
     
     try {
-        if (!mongoose.isValidObjectId(noteId)) {
-            throw createHttpError(400, "Invalid note ID");
-        }
-        
+        checkValidObjectId(noteId);
+
         const note = await NoteModel.findById(noteId).exec();
-        
         if (!note) {
             throw createHttpError(404, "Note not found");
         }
-        
+
         res.status(200).json(note);
     } catch(error) {
         next(error);
@@ -46,9 +44,7 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
     const color = req.body.color;
 
     try {
-        if (!title) {
-            throw createHttpError(400, "Note must have a title")
-        }
+        checkIfNoteHasTitle(title);
 
         const newNote = await NoteModel.create({
             title: title,
@@ -63,3 +59,82 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
         next(error);
     }
 };
+
+interface UpdateNoteParams {
+    noteId: string,
+}
+
+interface UpdateNoteBody {
+    title?: string,
+    text?: string,
+    tag?: string,
+    color?: string
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async(req, res, next) => {
+    const noteId = req.params.noteId;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+    const newTag = req.body.tag;
+    const newColor = req.body.color;
+    
+    try {
+        checkValidObjectId(noteId);
+        checkIfNoteHasTitle(newTitle);
+        
+        const note = await NoteModel.findById(noteId).exec();
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        note.title = newTitle!;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        note.text = newText!;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        note.tag = newTag!;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        note.color = newColor!;
+
+        const updatedNote = await note.save();
+
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+
+    try {
+        checkValidObjectId(noteId);
+        
+        const note = await NoteModel.findById(noteId).exec();
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        await note.deleteOne();
+
+        res.sendStatus(204);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+// Shorthand for validating object IDs
+function checkValidObjectId(noteId: string): void {
+    if (!mongoose.isValidObjectId(noteId)) {
+        throw createHttpError(400, "Invalid note ID");
+    }
+}
+
+// Shorthand for checking if a note has a title
+function checkIfNoteHasTitle(title: unknown) : void {
+    if (!title) {
+        throw createHttpError(400, "Note must have a title");
+    }
+} 
